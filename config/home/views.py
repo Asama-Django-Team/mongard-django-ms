999d29c40d419f6aa6f7430f7040324b41d4faa9
matinsajadii <emtinsajadi79@gmail.com>
2024-01-07 23:48:53 +0330

    update code trainig course mongard

--- config/home/views.py
+++ config/home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from home.models import Todo
+from django.contrib import messages
+from home.forms import TodoCreateForm, TodoUpdateForm
 
 # Create your views here.
 
@@ -14,4 +16,63 @@ def todo(request):
 
 def detail(request, todo_id):
     todo_item = Todo.objects.get(id=todo_id)
-    return render(request, "detail.html", {"item": todo_item})
+    return render(request, "detail.html", {"todo_item": todo_item})
+
+def delete(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    messages.success(request, "Todo is Deleted Successfully", extra_tags="warning")
+    return redirect("todo")
+
+def create(request):
+    if request.method == "POST":
+        form = TodoCreateForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Todo.objects.create(
+                title=cd["title"],
+                description=cd["description"],
+                date=cd["date"],
+                completed=cd["completed"]
+            )
+            messages.success(request, "Todo is Created Successfully", extra_tags="success")
+
+            return redirect("todo")
+    else:
+        form = TodoCreateForm()
+    return render(request, "create.html", {"form": form})
+
+def update(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    if request.method == "POST":
+        form = TodoUpdateForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Todo is Updated Successfully", extra_tags="success")
+
+            return redirect("details", todo_id)
+    else:
+        form = TodoUpdateForm(instance=todo)
+    return render(request, "update.html", {"form": form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
