fc7269ea261f0c27a2fcb872893307af2d7a5140
matinsajadii <emtinsajadi79@gmail.com>
2024-01-09 10:49:35 +0330

    update trainig code mongard

---
+++ config/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
